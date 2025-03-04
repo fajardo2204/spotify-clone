0fe2af0897374ded19a94cc0eb830a6f1623b0bf
fajardo2204 <juanfa_2204@hotmail.es>
2025-03-04 15:21:39 -0500

    useRef agregado a Player.jsx para funcionalidad de audio, se agregan parametros al componente dentro del layout para generar persistencia en la reproduccion de las canciones

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,28 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 // import { Pause } from "@/icons/Pause.astro";
 // import { Play } from "@/icons/Play.astro";
 
-const Pause = () => (
-  <svg viewBox="0 0 24 24" class="h-8 w-8" fill="currentColor"><path fill="currentColor" d="M8 5.14v14l11-7-11-7z"></path></svg>
+const Play = () => (
+  <svg viewBox="0 0 24 24" className="h-8 w-8" fill="currentColor"><path fill="currentColor" d="M8 5.14v14l11-7-11-7z"></path></svg>
 );
 
-const Play = () => (
-  <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
+const Pause = () => (
+  <svg role="img" height="24" width="24" aria-hidden="true" viewBox="0 0 16 16" className="h-8 w-8"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
 );
 
 export function Player() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentSong, setCurrentSong] = useState(null);
-  // const audioRef = useRef();
+  const audioRef = useRef();
+
+  useEffect(() => {
+    audioRef.current.src = '/music/1/01.mp3';
+  }, []);
 
   const handleClick = () => {
     if (isPlaying) {
       audioRef.current.pause();
     }
     else {
-      audioRef.current.src = `/music/1/01/mp3`;
       audioRef.current.play();
+      audioRef.current.volume = 0.5;
     }
 
     setIsPlaying(!isPlaying);
@@ -36,15 +40,17 @@ export function Player() {
       
       <div className="grid place-content-center gap-4 flex-1">
         <div className="flex justify-center">
-          <button className="bg-white rounded-full p-2" onClick={handleClick}>
+          <button className="bg-white rounded-full p-2" onClick={ handleClick }>
             {isPlaying ? <Pause /> : <Play />}
           </button>
         </div>
       </div>
       
       <div>
-        Actions
+        Volume...
       </div>
+
+      <audio ref={audioRef} />
     </div>
   );
 }
