58470aa978225864aad35f0f1839f2e509ce0f10
fajardo2204 <juanfa_2204@hotmail.es>
2025-03-04 14:37:17 -0500

    importaciones corregidas para legibilidad, modificaciones agregadas a las clases de tailwind para corregir estilos dentro de los componentes

--- src/components/Greeting.svelte
+++ src/components/Greeting.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   const currentTime = new Date();
   const currentHour = currentTime.getHours();
 
---
+++ src/components/Player.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+// import { Pause } from "@/icons/Pause.astro";
+// import { Play } from "@/icons/Play.astro";
+
+const Pause = () => (
+  <svg viewBox="0 0 24 24" class="h-8 w-8" fill="currentColor"><path fill="currentColor" d="M8 5.14v14l11-7-11-7z"></path></svg>
+);
+
+const Play = () => (
+  <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
+);
+
+export function Player() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentSong, setCurrentSong] = useState(null);
+  // const audioRef = useRef();
+
+  const handleClick = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    }
+    else {
+      audioRef.current.src = `/music/1/01/mp3`;
+      audioRef.current.play();
+    }
+
+    setIsPlaying(!isPlaying);
+  }
+
+  return (
+    <div className="flex flex-row justify-between w-full px-4 z-50">
+      <div>
+        CurrentSong...
+      </div>
+      
+      <div className="grid place-content-center gap-4 flex-1">
+        <div className="flex justify-center">
+          <button className="bg-white rounded-full p-2" onClick={handleClick}>
+            {isPlaying ? <Pause /> : <Play />}
+          </button>
+        </div>
+      </div>
+      
+      <div>
+        Actions
+      </div>
+    </div>
+  );
+}
